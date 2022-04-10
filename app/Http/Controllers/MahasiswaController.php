ce5dc67d593ccad398b24b66f393f58bbc411723
Dka-11 <adikaahmad05@gmail.com>
2022-04-10 21:17:26 +0700

    Add Controller

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mahasiswa = Mahasiswa::all(); //Mengambil semua isi data
+        $paginate = Mahasiswa::orderBy('nim', 'asc')->paginate(3);
+        return view('mahasiswa.index', [
+            'mahasiswa' => $mahasiswa,
+            'paginate' => $paginate
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validasi Data
+        $request->validate([
+            'Nim' => 'required',
+            'Nama' => 'required',
+            'Kelas' => 'required',
+            'Jurusan' => 'required'
+        ]);
+        // End Validasi Data
+
+        //Eloquent untuk menambah data.
+        Mahasiswa::create($request->all());
+        return redirect()->route('mahasiswa.index') //jika data berhasil ditambahkan kembali ke hal. utama
+            ->with('success', 'Mahasiswa Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($Nim)
+    {
+        $Mahasiswa = Mahasiswa::where('nim', $Nim)->first(); //Menampilkan data dengan menemukan nim mahasiswa
+        return view('mahasiswa.detail', compact('Mahasiswa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($Nim)
+    {
+        //Menampilkan data dengan menemukan nim yang pertama untuk diedit
+        $Mahasiswa = DB::table('mahasiswa')->where('Nim', $Nim)->first();
+        return view('mahasiswa.edit', compact('Mahasiswa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $Nim)
+    {
+        //Validasi data
+        $request->validate([
+            'Nim' => 'required',
+            'Nama' => 'required',
+            'Kelas' => 'required',
+            'Jurusan' => 'required'
+        ]);
+
+        Mahasiswa::where('nim', $Nim)->update($request->except(['_token', '_method'])); //Mengambil semua data untuk di edit/update
+        return redirect()->route('mahasiswa.index')
+            ->with('success', 'Mahasiswa Berhasil Diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($Nim)
+    {
+        Mahasiswa::where('nim', $Nim)->delete();
+        return redirect()->route('mahasiswa.index')
+            ->with('success', 'Mahasiswa Berhasil Dihapus');
+    }
+}
